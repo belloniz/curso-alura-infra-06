7a810d56d445f84f35e7af5534e9e85c92e604db
Gabriel Belloni <gabriel.belloni@outlook.com>
2023-03-30 00:34:45 -0300

    Update Backend.tf

--- env/Homolog/Backend.tf
+++ env/Homolog/Backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-curso-alura-info-06	"
+    bucket = "terraform-state-curso-alura-info-06"
     key    = "Prod/terraform.tfstate"
     region = "us-east-1"
   }
